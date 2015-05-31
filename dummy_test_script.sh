8e22e9deccb75a6f5d062ba13a61e60ca4209c48
maaaato <igosso.mato@gmail.com>
2015-06-01 00:45:21 +0900

    slackへの通知をスクリプト使わない

--- dummy_test_script.sh
+++ dummy_test_script.sh
@@ -2,4 +2,4 @@
 
 RESULT=`terraform plan`
 
-curl -X POST --data-urlencode 'payload={"channel": "#general", "username": "pullreq-bot", "text": ${RESULT}}' https://hooks.slack.com/services/${circleci_for_slack}
+curl -X POST --data-urlencode 'payload={"channel": "#general", "username": "pullreq-bot", "text": "$RESULT"}' https://hooks.slack.com/services/${circleci_for_slack}
