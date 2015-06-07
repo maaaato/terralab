b20485603d0987d8737cc5271e2d21506f739136
maaaato <igosso.mato@gmail.com>
2015-06-08 00:31:19 +0900

    curlの引数を修正

--- create_pull_request.sh
+++ create_pull_request.sh
@@ -3,7 +3,8 @@
 HEAD=${1}
 BASE=${2}
 TITLE="`date +"%Y.%m.%d"` ${HEAD} into ${BASE}"
+DATA='{"title":"'${TITLE}'","head":"'${HEAD}'","base":"'${BASE}'"}'
 
 curl -i -H "Authorization: token ${GITHUB_TOKEN}" \
-     -d '{"title":"${TITLE}","head":"${HEAD}","base":"${BASE}"}' \
+     -d "${DATA}" \
      https://api.github.com/repos/maaaato/terralab/pulls
