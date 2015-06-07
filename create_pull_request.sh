92e5361ba9e7918e0ff97426813c9ff8380d37f8
maaaato <igosso.mato@gmail.com>
2015-06-07 23:54:13 +0900

    pullreqのスクリプトを修正

--- create_pull_request.sh
+++ create_pull_request.sh
@@ -4,10 +4,10 @@ get_date() {
     date
 }
 
-TITLE=get_date
+TITLE=`get_date`
 HEAD=${1}
 BASE=${2}
 
 curl -i -H "Authorization: token ${GITHUB_TOKEN}" \
-     -d '{"title":$TITLE,"head":$HEAD,"base":"$BASE"}' \
+     -d '{"title":${TITLE}","head":${HEAD},"base":"${BASE}"}' \
      https://api.github.com/repos/maaaato/terralab/pulls
